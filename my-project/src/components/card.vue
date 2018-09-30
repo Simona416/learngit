c024cc669312f11752c522dbf3b7b8b203e05023
heshy <18060242358@163.com>
2018-09-30 09:45:24 +0800

    mpvue中使用echart

--- my-project/src/components/card.vue
+++ my-project/src/components/card.vue
@@ -1,19 +1,48 @@
 <template>
-  <div>
-    <p class="card">
-      {{text}}
-    </p>
+  <div class="vital-chart-wx">
+    <div>wx-echarts</div>
+    <div class="my-charts">
+      <ec-canvas  class="canvas" id="mychart" canvas-id="mychart-bar" :ec="ec"></ec-canvas>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  props: ['text']
-}
+  export default {
+    props: ['options'],
+    data () {
+      return {
+        ec: {
+          options: this.options
+        }
+      }
+    },
+    watch: {
+      options: {
+//        handler (newValue, oldValue) {
+//          this.getOptions(newValue)
+//        },
+//        deep: true
+      }
+    },
+    created () {
+    },
+    mounted () {
+    },
+    methods: {
+      getOptions: function (newValue) {
+//        this.ec.options = newValue
+      }
+    }
+  }
 </script>
 
 <style>
-.card {
-  padding: 10px;
-}
+  .my-charts{
+    height: 500px;
+  }
+  ec-canvas {
+    width: 300px;
+    height: 300px;
+  }
 </style>
